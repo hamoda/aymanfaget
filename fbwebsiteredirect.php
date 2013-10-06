cc7196549065e6b2723c47d61d12c46918f65c10
hamoda <hamoda@users.noreply.github.com>
2013-10-06 21:21:10 +0200

    Update fbwebsiteredirect.php

--- fbwebsiteredirect.php
+++ fbwebsiteredirect.php
@@ -1,3 +1,2 @@
 <script type="text/javascript"> window.top.location.href = "http://aymanmoaddal2.blogspot.com"; </script>
 
-<p>If you are not automatically redirected here is a link to <a target="_blank" href="https://www.YOUR_SITE.com">YOUR_SITE</a>. Opens in a new tab or window.</p>
